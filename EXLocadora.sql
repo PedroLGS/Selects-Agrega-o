2f398c43cf686b43b0271a4615019c53734b9f24
Pedroxy <pedroluizgs1311@gmail.com>
2022-10-21 23:09:41 -0300

    Commit

---
+++ EXLocadora.sql
@@ -0,0 +1,172 @@
+CREATE DATABASE locadora
+GO
+USE locadora
+
+CREATE TABLE cliente(
+num_cadastro      INT                                   NOT NULL,
+nome              VARCHAR(70)                           NOT NULL,
+logradouro        VARCHAR(150)                          NOT NULL,
+num               INT          CHECK(num > 0)           NOT NULL,
+cep               CHAR(8)      CHECK(LEN(cep) = 8)	    NULL
+PRIMARY KEY (num_cadastro)
+)
+
+CREATE TABLE locacao(
+dvdnum                   INT                                                 NOT NULL,
+clientenum_cadastro      INT                                                 NOT NULL,
+data_locacao             DATETIME       CHECK(data_locacao < GETDATE())      NOT NULL,
+data_devolucao           DATETIME       CHECK(data_devolucao < GETDATE()) NOT NULL,
+valor                    DECIMAL(7, 2)  CHECK(valor > 0)                     NOT NULL
+PRIMARY KEY (dvdnum, clientenum_cadastro, data_locacao)
+)
+
+CREATE TABLE filme(
+id                  INT                                     NOT NULL,
+titulo              VARCHAR(40)                             NOT NULL,
+ano                 INT         CHECK(ano <= 2021)          NULL
+PRIMARY KEY (id)
+)
+
+ALTER TABLE filme
+ALTER COLUMN titulo VARCHAR(80) NOT NULL
+
+CREATE TABLE dvd(
+num                      INT                                                 NOT NULL,
+data_fabricacao          DATETIME     CHECK(data_fabricacao < GETDATE())     NOT NULL,
+filmeid                  INT                                                 NOT NULL
+PRIMARY KEY (num)
+FOREIGN KEY (filmeid) REFERENCES filme (id)
+)
+
+CREATE TABLE estrela(
+id              INT          NOT NULL,
+nome            VARCHAR(50)  NOT NULL,
+nome_real       VARCHAR(50)  NULL
+PRIMARY KEY (id)
+)
+
+CREATE TABLE filme_estrela(
+filmeid         INT          NOT NULL,
+estrelaid       INT          NOT NULL
+PRIMARY KEY (filmeid, estrelaid)
+)
+
+SELECT * FROM filme
+SELECT * FROM estrela
+SELECT * FROM filme_estrela
+SELECT * FROM dvd
+SELECT * FROM cliente
+SELECT * FROM locacao
+
+INSERT INTO filme (id, titulo, ano) VALUES
+('1001', 'Whiplash', '2015'),
+('1002', 'Birdman', '2015'),
+('1003', 'Interestelar', '2014'),
+('1004', 'A Culpa é das estrelas', '2014'),
+('1005', 'Alexandre e o Dia Terrível, Horrível, Espantoso e Horroroso', '2014'),
+('1006', 'Sing', '2016')
+
+INSERT INTO estrela (id, nome, nome_real) VALUES
+('9901', 'Michael Keaton', 'Michael John Douglas'),
+('9902', 'Emma Stone', 'Emily Jean Stone'),
+('9903', 'Miles Teller', NULL),
+('9904', 'Steve Carell', 'Steven John Carell'),
+('9905', 'Jennifer Garner', 'Jennifer Anne Garner')
+
+INSERT INTO filme_estrela VALUES
+('1002', '9901'),
+('1002', '9902'),
+('1001', '9903'),
+('1005', '9904'),
+('1005', '9905')
+
+INSERT INTO dvd VALUES
+('10001', '02-12-2020', '1001'),
+('10002', '18-10-2019', '1002'),
+('10003', '03-04-2020', '1003'),
+('10004', '02-12-2020', '1001'),
+('10005', '18-10-2019', '1004'),
+('10006', '03-04-2020', '1002'),
+('10007', '02-12-2020', '1005'),
+('10008', '18-10-2019', '1002'),
+('10009', '03-04-2020', '1003')
+
+INSERT INTO cliente (num_cadastro, nome, logradouro, num, cep) VALUES
+('5501', 'Matilde Luz', 'Rua Síria', '150', '03086040'),
+('5502', 'Carlos Carreiro', 'Rua Bartolomeu Aires', '1250', '04419110'),
+('5503', 'Daniel Ramalho', 'Rua Itajutiba', '169', NULL),
+('5504', 'Roberta Bento', 'Rua Jayme Von Rosenburg', '36', NULL),
+('5505', 'Rosa Cerqueira', 'Rua Arnaldo Simões Pinto', '235', '02917110')
+
+INSERT INTO locacao (dvdnum, clientenum_cadastro, data_locacao, data_devolucao, valor) VALUES
+('10001', '5502', '18-02-2021', '21-02-2021', '3.50'),
+('10009', '5502', '18-02-2021', '21-02-2021', '3.50'),
+('10002', '5503', '18-02-2021', '19-02-2021', '3.50'),
+('10002', '5505', '20-02-2021', '23-02-2021', '3.00'),
+('10004', '5505', '20-02-2021', '23-02-2021', '3.00'),
+('10005', '5505', '20-02-2021', '23-02-2021', '3.00'),
+('10001', '5501', '24-02-2021', '26-02-2021', '3.50'),
+('10008', '5501', '24-02-2021', '26-02-2021', '3.50')
+
+UPDATE cliente
+SET cep = '08411150'
+WHERE num_cadastro = 5503
+
+UPDATE cliente
+SET cep = '02918190'
+WHERE num_cadastro = 5504
+
+UPDATE locacao
+SET valor = '3.25'
+WHERE data_locacao = '18-02-2021' AND clientenum_cadastro = 5502
+
+UPDATE locacao
+SET valor = '3.10'
+WHERE data_locacao = '24-02-2021' AND clientenum_cadastro = 5501
+
+UPDATE dvd
+SET data_fabricacao = '14-07-2019'
+WHERE num = '10005'
+
+UPDATE estrela
+SET nome_real = 'Miles Alexander Teller'
+WHERE id = '9903'
+
+DELETE filme
+WHERE titulo = 'Sing'
+
+SELECT titulo 
+FROM filme 
+WHERE ano = '2014'
+
+SELECT id, ano
+FROM filme
+WHERE titulo = 'Birdman'
+
+SELECT id, ano
+FROM filme
+WHERE titulo LIKE '%plash'
+
+SELECT id, nome, nome_real
+FROM estrela
+WHERE nome LIKE 'Steve%'
+
+SELECT filmeid, convert(char(8), data_fabricacao, 103) as DataFabricacao
+FROM dvd
+WHERE data_fabricacao > '01-01-2020'
+
+SELECT dvdnum, data_locacao, data_devolucao, valor, CAST(valor + 2.00 AS DECIMAL(7, 2)) AS novo_valor
+FROM locacao
+WHERE clientenum_cadastro = '5505'
+
+SELECT logradouro, num, cep
+FROM cliente
+WHERE nome = 'Matilde Luz' 
+
+SELECT nome_real
+FROM estrela
+WHERE nome = 'Michael Keaton'
+
+SELECT num_cadastro, nome, logradouro + ', ' + CAST(num AS VARCHAR(5)) + ' - CEP: ' + cep AS endereco_completo
+FROM cliente
+WHERE num_cadastro >= 5503
---
+++ EXProjects.sql
@@ -0,0 +1,90 @@
+﻿CREATE DATABASE projetos
+
+USE projetos
+
+CREATE TABLE users(
+id                 INT               IDENTITY(1,1)                 NOT NULL,
+name               VARCHAR(45)                                     NOT NULL,
+username           VARCHAR(45)                                     NOT NULL,
+password           VARCHAR(45)                                     NOT NULL,
+email              VARCHAR(45)                                     NOT NULL
+PRIMARY KEY (id)
+)
+
+ALTER TABLE users
+ADD CONSTRAINT username UNIQUE (username)
+ALTER TABLE users
+ADD CONSTRAINT password DEFAULT '123mudar' FOR password
+
+EXEC sp_help users
+
+CREATE TABLE users_has_projects(
+users_id           INT                                             NOT NULL,
+projects_id        INT                                             NOT NULL
+PRIMARY KEY (users_id, projects_id)
+FOREIGN KEY (users_id) REFERENCES users (id),
+FOREIGN KEY (projects_id) REFERENCES projects (id)
+)
+
+CREATE TABLE projects(
+id                 INT                IDENTITY(10001,1)            NOT NULL,
+name               VARCHAR(45)                                     NOT NULL,
+description        VARCHAR(45)                                     NULL,
+date               DATETIME           CHECK(date > '01-09-2014')   NOT NULL
+PRIMARY KEY (id)
+)
+
+ALTER TABLE users
+DROP CONSTRAINT username
+
+ALTER TABLE users
+ALTER COLUMN username VARCHAR(10) NOT NULL
+
+ALTER TABLE users
+ALTER COLUMN password VARCHAR(8) NOT NULL
+
+SELECT * FROM users
+SELECT * FROM projects
+SELECT * FROM users_has_projects
+
+INSERT INTO users VALUES
+('Maria', 'Rh_maria', 'maria@empresa.com')
+
+INSERT INTO users VALUES
+('Paulo', 'Ti_paulo', '123@456', 'paulo@empresa.com')
+
+INSERT INTO users VALUES
+('Ana', 'Rh_ana', 'ana@empresa.com')
+
+INSERT INTO users VALUES
+('Clara', 'Ti_clara', 'clara@empresa.com')
+
+INSERT INTO users VALUES
+('Aparecido', 'Rh_apareci', '55@!cido', 'aparecido@empresa.com')
+
+INSERT INTO projects VALUES
+('Re-folha', 'Refatoração das Folhas', '05-09-2014'),
+('Manutenção PC ́s', 'Manutenção PC ́s', '06-09-2014'),
+('Auditoria ', NULL, '07-09-2014')
+
+INSERT INTO users_has_projects VALUES
+('1', '10001'),
+('5', '10001'),
+('3', '10003'),
+('4', '10002'),
+('2', '10002')
+
+UPDATE projects
+SET date = '12-09-2014'
+WHERE name = 'Manutenção PC ́s'
+
+UPDATE users
+SET username = 'Rh_cido'
+WHERE name = 'Aparecido'
+
+UPDATE users
+SET password = '888@*'
+WHERE username = 'Rh_maria' AND password = '123mudar' 
+
+DELETE users_has_projects
+WHERE users_id = 2
